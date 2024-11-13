f28910cc77a29789a0732a34316857fd55c697da
Samuel Warrie <samuelwarrie735@gmail.com>
2024-11-14 00:24:52 +0200

    created the search bar and its necessary functionalities

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {SearchBar} from './components/SearchBar.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h3>Movie app</h3>
+      <div className="search-bar-container">
+        <SearchBar />
+      {/* <h3>Movie app</h3> */}
+        </div>
     </div>
   );
 }
