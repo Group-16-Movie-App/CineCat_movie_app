f5e4d980d8c3eb9ad6e705b9f82553fc52c8e951
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-11-15 09:25:54 +0200

    Create sign-up form with email and password validation

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SignUpForm } from './components/SignUpForm';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Movie app</h3>
-    </div>
+    <Router>
+      <div className="App">
+        <h3>Movie app</h3>
+        <Routes>
+          <Route path="/signup" element={<SignUpForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
