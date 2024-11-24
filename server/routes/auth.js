cae25d69c1eb88c7f4f3f5df9ab3d46c131d03a6
Ville Kahelin <t3kavi02@students.oamk.fi>
2024-11-24 20:20:14 +0200

    First commit delete account feature

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { register, login, logout } from '../controllers/authController.js';
+import { register, login, logout, deleteAccount } from '../controllers/authController.js';
+import { authenticateToken } from '../config/middleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
+router.delete('/account', authenticateToken, deleteAccount);
 
 export default router;
