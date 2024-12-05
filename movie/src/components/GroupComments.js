3fc1fa7d2b98b559343941b85e6ceaa7d364972f
Gloryozo <gloryozoji@gmail.com>
2024-12-05 13:29:12 +0200

    Added Like and reply buttons to group comments

--- movie/src/components/GroupComments.js
+++ movie/src/components/GroupComments.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const GroupComments = ({ groupId }) => {
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
+    const [replies, setReplies] = useState({});
 
     const fetchComments = async () => {
         const token = localStorage.getItem('token');
@@ -25,14 +26,29 @@ const GroupComments = ({ groupId }) => {
         }
 
         try {
-           const response = await axios.post(`http://localhost:5000/api/groups/${groupId}/comments`, { text: newComment }, {
+            const response = await axios.post(`http://localhost:5000/api/groups/${groupId}/comments`, { text: newComment }, {
                 headers: { Authorization: `Bearer ${token}` }
-             });
-             console.log('comment added:', response.data); 
+            });
+            console.log('Comment added:', response.data);
             setNewComment('');
             fetchComments();
         } catch (error) {
             console.error('Error adding comment:', error);
+        }
+    };
+
+    const handleLikeComment = (commentId) => {
+        console.log(`Liked comment with ID: ${commentId}`);
+    };
+
+    const handleReplyComment = (commentId) => {
+        const replyText = prompt('Enter your reply:');
+        if (replyText) {
+            console.log(`Reply to comment ${commentId}: ${replyText}`);
+            setReplies((prev) => ({
+                ...prev,
+                [commentId]: [...(prev[commentId] || []), replyText]
+            }));
         }
     };
 
@@ -45,7 +61,18 @@ const GroupComments = ({ groupId }) => {
             <h3>Comments</h3>
             <ul>
                 {comments.map(comment => (
-                    <li key={comment.id}>{comment.text}</li>
+                    <li key={comment.id}>
+                        {comment.text}
+                        <button onClick={() => handleLikeComment(comment.id)}>Like</button>
+                        <button onClick={() => handleReplyComment(comment.id)}>Reply</button>
+                        {replies[comment.id] && replies[comment.id].length > 0 && (
+                            <ul>
+                                {replies[comment.id].map((reply, index) => (
+                                    <li key={index}>{reply}</li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
                 ))}
             </ul>
             <textarea
