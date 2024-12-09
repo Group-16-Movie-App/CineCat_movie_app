e10ba162675819bd1faae9d3b8f7b6757bf58dd4
ankit <t3raan02@students.oamk.fi>
2024-12-09 18:27:09 +0200

    stylingchange

--- movie/src/pages/SearchPage.js
+++ movie/src/pages/SearchPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieList from '../components/MovieList';
 import SearchForm from '../components/SearchForm';
+import '../components/SearchBar.css';
 
 
 const SearchPage = () => {
