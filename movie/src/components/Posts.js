a2a4a0685a6261aa9ed103393422ba201c67c5ae
Danh Do <t3dodu00@students.oamk.fi>
2024-12-12 15:09:43 +0200

    update

--- movie/src/components/Posts.js
+++ movie/src/components/Posts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import './Posts.css'; // Import your CSS file
+import './Posts.css'; 
 
 const Posts = ({ groupId }) => {
     const [posts, setPosts] = useState([]);
